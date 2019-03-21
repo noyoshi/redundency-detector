df2828a47e92953194ef0b01b57812d202bd93f4
Noah Yoshida <nyoshida@nd.edu>
2019-03-21 13:50:50 -0700

    derp

---
+++ config.h
@@ -0,0 +1,28 @@
+/* debug.h: Debugging Macros */
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define HASHTABLE_SIZE 20000
+#define DATA_SIZE 2400
+
+#define debug(M, ...) \
+    fprintf(stderr, "DEBUG %s:%d:%s: " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+
+#define ERROR \
+    { \
+    fprintf(stderr, "[ERROR] %s\n", strerror(errno)); \
+    debug(); \
+    exit(EXIT_FAILURE); \
+    }
+
+#define check(r) \
+    if (r < 0) {\
+        ERROR;\
+    }\
+
+
